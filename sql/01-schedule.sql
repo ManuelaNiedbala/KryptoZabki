c7e9565632fca6fcf55b87a6fcb2a1bc2a4429ed
nm53878 <nm53878@zut.edu.pl>
2025-01-15 19:41:34 +0100

    Zmiany w bazie

--- sql/01-schedule.sql
+++ sql/01-schedule.sql
@@ -7,44 +7,45 @@ DROP TABLE IF EXISTS rooms;
 DROP TABLE IF EXISTS schedules;
 CREATE TABLE faculty (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    faculty_name VARCHAR(255) NOT NULL
+    faculty_name TEXT NOT NULL
 );
 CREATE TABLE room (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    room_name VARCHAR(255) NOT NULL,
+    room_name TEXT NOT NULL,
     faculty_id INTEGER NOT NULL,
     FOREIGN KEY (faculty_id) REFERENCES faculty(id)
 );
 CREATE TABLE group (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    group_name VARCHAR(255) NOT NULL,
-    faculty_id INTEGER NOT NULL,
-    FOREIGN KEY (faculty_id) REFERENCES faculty(id)
+    group_name TEXT NOT NULL
+);
+CREATE TABLE group_student (
+    group_id INTEGER NOT NULL,
+    student_id INTEGER NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES group(id),
+    FOREIGN KEY (student_id) REFERENCES student(id)
 );
 CREATE TABLE subject (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    subject_name VARCHAR(255) NOT NULL,
-    subject_form VARCHAR(255) NOT NULL
+    subject_name TEXT NOT NULL,
+    subject_form TEXT NOT NULL
 );
 CREATE TABLE lecturer (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    lecturer_name VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL
+    lecturer_name TEXT NOT NULL,
+    title TEXT NOT NULL
 );
-CREATE TABLE student (
-    id INTEGER PRIMARY KEY,
-    group_id INTEGER,
-    FOREIGN KEY (group_id) REFERENCES group(id)
-);
+CREATE TABLE student (id INTEGER PRIMARY KEY);
 CREATE TABLE schedules (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    subject_id INTEGER,
-    lecturer_id INTEGER,
-    faculty_id INTEGER,
-    group_id INTEGER,
-    room_id INTEGER,
-    time_start DATETIME,
-    time_end DATETIME,
+    subject_id INTEGER NOT NULL,
+    lecturer_id INTEGER NOT NULL,
+    faculty_id INTEGER NOT NULL,
+    group_id INTEGER NOT NULL,
+    room_id INTEGER NOT NULL,
+    time_start TEXT NOT NULL,
+    time_end TEXT NOT NULL,
+    color TEXT,
     FOREIGN KEY (subject_id) REFERENCES subject(id),
     FOREIGN KEY (lecturer_id) REFERENCES lecturer(id),
     FOREIGN KEY (faculty_id) REFERENCES faculty(id),
